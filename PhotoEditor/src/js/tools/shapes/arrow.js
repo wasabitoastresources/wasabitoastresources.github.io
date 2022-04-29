d8d351f1ee43683a49aee894a0560ed1c404dbf8
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 12:49:49 -0500

    Update arrow.js

--- PhotoEditor/src/js/tools/shapes/arrow.js
+++ PhotoEditor/src/js/tools/shapes/arrow.js
@@ -2,9 +2,7 @@ import app from './../../app.js';
 import config from './../../config.js';
 import Base_tools_class from './../../core/base-tools.js';
 import Base_layers_class from './../../core/base-layers.js';
-
 class Arrow_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
 		this.Base_layers = new Base_layers_class();
@@ -15,21 +13,16 @@ class Arrow_class extends Base_tools_class {
 		this.snap_line_info = {x: null, y: null};
 		this.mouse_click = {x: null, y: null};
 	}
-
 	load() {
 		this.default_events();
 	}
-
 	mousedown(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.click_valid == false) {
 			return;
 		}
-
 		var mouse_x = mouse.x;
 		var mouse_y = mouse.y;
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -39,11 +32,8 @@ class Arrow_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		this.mouse_click.x = mouse_x;
 		this.mouse_click.y = mouse_y;
-
-		//register new object - current layer is not ours or params changed
 		this.layer = {
 			type: this.name,
 			params: this.clone(this.getParams()),
@@ -61,7 +51,6 @@ class Arrow_class extends Base_tools_class {
 			])
 		);
 	}
-
 	mousemove(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.is_drag == false)
@@ -69,13 +58,10 @@ class Arrow_class extends Base_tools_class {
 		if (mouse.click_valid == false) {
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -85,37 +71,28 @@ class Arrow_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		var width = mouse_x - this.layer.x;
 		var height = mouse_y - this.layer.y;
 		if (e.ctrlKey == true || e.metaKey) {
-			//one direction only
 			if (Math.abs(width) < Math.abs(height))
 				width = 0;
 			else
 				height = 0;
 		}
-
-		//more data
 		config.layer.width = width;
 		config.layer.height = height;
-
 		this.Base_layers.render();
 	}
-
 	mouseup(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.click_valid == false) {
 			config.layer.status = null;
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -126,25 +103,18 @@ class Arrow_class extends Base_tools_class {
 			}
 		}
 		this.snap_line_info = {x: null, y: null};
-
 		var width = mouse_x - this.layer.x;
 		var height = mouse_y - this.layer.y;
-
 		if (width == 0 && height == 0) {
-			//same coordinates - cancel
 			app.State.scrap_last_action();
 			return;
 		}
-
 		if (e.ctrlKey == true || e.metaKey) {
-			//one direction only
 			if (Math.abs(width) < Math.abs(height))
 				width = 0;
 			else
 				height = 0;
 		}
-
-		//more data
 		app.State.do_action(
 			new app.Actions.Update_layer_action(config.layer.id, {
 				width,
@@ -154,35 +124,26 @@ class Arrow_class extends Base_tools_class {
 			{ merge_with_history: 'new_line_layer' }
 		);
 	}
-
 	render_overlay(ctx){
 		var ctx = this.Base_layers.ctx;
 		this.render_overlay_parent(ctx);
 	}
-
 	demo(ctx, x, y, width, height) {
 		ctx.fillStyle = '#aaa';
 		ctx.strokeStyle = '#555';
 		ctx.lineWidth = 2;
-
 		this.arrow(ctx, x, y, x + width, y + height, 15);
 	}
-
 	render(ctx, layer) {
 		if (layer.width == 0 && layer.height == 0)
 			return;
-
 		var params = layer.params;
-
-		//set styles
 		ctx.fillStyle = layer.color;
 		ctx.strokeStyle = layer.color;
 		ctx.lineWidth = params.size;
 		ctx.lineCap = 'round';
-
 		var width = layer.x + layer.width;
 		var height = layer.y + layer.height;
-
 		var headlen = params.size * 7;
 		if (headlen < 15)
 			headlen = 15;
@@ -191,7 +152,6 @@ class Arrow_class extends Base_tools_class {
 			width, height,
 			headlen);
 	}
-
 	arrow(ctx, fromx, fromy, tox, toy, headlen) {
 		var dx = tox - fromx;
 		var dy = toy - fromy;
@@ -206,7 +166,5 @@ class Arrow_class extends Base_tools_class {
 		ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
 		ctx.stroke();
 	}
-
 }
-
 export default Arrow_class;
