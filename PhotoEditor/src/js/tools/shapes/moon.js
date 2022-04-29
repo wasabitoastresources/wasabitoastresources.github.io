c220ebac4e818edf52677984323c01def2d61a15
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 13:00:05 -0500

    Update moon.js

--- PhotoEditor/src/js/tools/shapes/moon.js
+++ PhotoEditor/src/js/tools/shapes/moon.js
@@ -1,8 +1,6 @@
 import Base_tools_class from './../../core/base-tools.js';
 import Base_layers_class from './../../core/base-layers.js';
-
 class Moon_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
 		this.Base_layers = new Base_layers_class();
@@ -12,48 +10,37 @@ class Moon_class extends Base_tools_class {
 		this.best_ratio = 0.8;
 		this.snap_line_info = {x: null, y: null};
 	}
-
 	load() {
 		this.default_events();
 	}
-
 	mousedown(e) {
 		this.shape_mousedown(e);
 	}
-
 	mousemove(e) {
 		this.shape_mousemove(e);
 	}
-
 	mouseup(e) {
 		this.shape_mouseup(e);
 	}
-
 	render_overlay(ctx){
 		var ctx = this.Base_layers.ctx;
 		this.render_overlay_parent(ctx);
 	}
-
 	demo(ctx, x, y, width, height) {
 		ctx.fillStyle = '#aaa';
 		ctx.strokeStyle = '#555';
 		ctx.lineWidth = 2;
-
 		var width_all = width + x * 2;
 		width = height * this.best_ratio;
 		x = (width_all - width) / 2;
-
 		ctx.save();
 		ctx.translate(x + width / 2, y + height / 2);
 		this.draw_shape(ctx, -width / 2, -height / 2, width, height, true, true);
 		ctx.restore();
 	}
-
 	render(ctx, layer) {
 		var params = layer.params;
 		ctx.save();
-
-		//set styles
 		ctx.strokeStyle = 'transparent';
 		ctx.fillStyle = 'transparent';
 		if(params.border)
@@ -61,19 +48,14 @@ class Moon_class extends Base_tools_class {
 		if(params.fill)
 			ctx.fillStyle = params.fill_color;
 		ctx.lineWidth = params.border_size;
-
-		//draw with rotation support
 		ctx.translate(layer.x + layer.width / 2, layer.y + layer.height / 2);
 		ctx.rotate(layer.rotate * Math.PI / 180);
 		this.draw_shape(ctx, -layer.width / 2, -layer.height / 2, layer.width, layer.height, params.fill, params.border);
-
 		ctx.restore();
 	}
-
 	draw_shape(ctx, x, y, width, height, fill, stroke) {
 		var left = parseInt(x);
 		var top = parseInt(y);
-
 		ctx.beginPath();
 		ctx.moveTo(left + width * 0.512, top + height / 2);
 		ctx.bezierCurveTo(
@@ -114,7 +96,5 @@ class Moon_class extends Base_tools_class {
 			ctx.stroke();
 		}
 	}
-
 }
-
 export default Moon_class;
