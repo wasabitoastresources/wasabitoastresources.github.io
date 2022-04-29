d6067423d362b3bcaf65ee14fd47e4da25882a2e
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 12:59:18 -0500

    Update line.js

--- PhotoEditor/src/js/tools/shapes/line.js
+++ PhotoEditor/src/js/tools/shapes/line.js
@@ -2,9 +2,7 @@ import app from './../../app.js';
 import config from './../../config.js';
 import Base_tools_class from './../../core/base-tools.js';
 import Base_layers_class from './../../core/base-layers.js';
-
 class Line_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
 		this.Base_layers = new Base_layers_class();
@@ -15,20 +13,15 @@ class Line_class extends Base_tools_class {
 		this.snap_line_info = {x: null, y: null};
 		this.mouse_click = {x: null, y: null};
 	}
-
 	load() {
 		this.default_events();
 	}
-
 	mousedown(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.click_valid == false)
 			return;
-
 		var mouse_x = mouse.x;
 		var mouse_y = mouse.y;
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -38,11 +31,8 @@ class Line_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		this.mouse_click.x = mouse_x;
 		this.mouse_click.y = mouse_y;
-
-		//register new object - current layer is not ours or params changed
 		this.layer = {
 			type: this.name,
 			params: this.clone(this.getParams()),
@@ -60,7 +50,6 @@ class Line_class extends Base_tools_class {
 			])
 		);
 	}
-
 	mousemove(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.is_drag == false)
@@ -68,13 +57,10 @@ class Line_class extends Base_tools_class {
 		if (mouse.click_valid == false) {
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -84,37 +70,28 @@ class Line_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		var width = mouse_x - this.layer.x;
 		var height = mouse_y - this.layer.y;
 		if (e.ctrlKey == true || e.metaKey) {
-			//one direction only
 			if (Math.abs(width) < Math.abs(height))
 				width = 0;
 			else
 				height = 0;
 		}
-
-		//more data
 		config.layer.width = width;
 		config.layer.height = height;
-
 		this.Base_layers.render();
 	}
-
 	mouseup(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.click_valid == false) {
 			config.layer.status = null;
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -125,26 +102,18 @@ class Line_class extends Base_tools_class {
 			}
 		}
 		this.snap_line_info = {x: null, y: null};
-
-
 		var width = mouse_x - this.layer.x;
 		var height = mouse_y - this.layer.y;
-
 		if (width == 0 && height == 0) {
-			//same coordinates - cancel
 			app.State.scrap_last_action();
 			return;
 		}
-
 		if (e.ctrlKey == true || e.metaKey) {
-			//one direction only
 			if (Math.abs(width) < Math.abs(height))
 				width = 0;
 			else
 				height = 0;
 		}
-
-		//more data
 		app.State.do_action(
 			new app.Actions.Update_layer_action(config.layer.id, {
 				width,
@@ -154,12 +123,10 @@ class Line_class extends Base_tools_class {
 			{ merge_with_history: 'new_line_layer' }
 		);
 	}
-
 	render_overlay(ctx){
 		var ctx = this.Base_layers.ctx;
 		this.render_overlay_parent(ctx);
 	}
-
 	demo(ctx, x, y, width, height) {
 		var coords = [
 			[0, 0],
@@ -167,29 +134,20 @@ class Line_class extends Base_tools_class {
 		];
 		this.draw_shape(ctx, x, y, width, height, coords);
 	}
-
 	render(ctx, layer) {
 		if (layer.width == 0 && layer.height == 0)
 			return;
-
 		var params = layer.params;
-
-		//set styles
 		ctx.fillStyle = layer.color;
 		ctx.strokeStyle = layer.color;
 		ctx.lineWidth = params.size;
 		ctx.lineCap = 'round';
-
 		var width = layer.x + layer.width;
 		var height = layer.y + layer.height;
-
-		//draw line
 		ctx.beginPath();
 		ctx.moveTo(layer.x, layer.y);
 		ctx.lineTo(width, height);
 		ctx.stroke();
 	}
-
 }
-
 export default Line_class;
