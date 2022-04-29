79433867028adb97afb820763dd8a86982c13971
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 10:22:17 -0500

    Update pencil.js

--- PhotoEditor/src/js/tools/pencil.js
+++ PhotoEditor/src/js/tools/pencil.js
@@ -2,9 +2,7 @@ import app from './../app.js';
 import config from './../config.js';
 import Base_tools_class from './../core/base-tools.js';
 import Base_layers_class from './../core/base-layers.js';
-
 class Pencil_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
 		this.Base_layers = new Base_layers_class();
@@ -12,13 +10,10 @@ class Pencil_class extends Base_tools_class {
 		this.layer = {};
 		this.params_hash = false;
 		this.pressure_supported = false;
-		this.pointer_pressure = 0; // has range [0 - 1]
+		this.pointer_pressure = 0;
 	}
-
 	load() {
 		var _this = this;
-
-		//pointer events
 		document.addEventListener('pointerdown', function (event) {
 			_this.pointerdown(event);
 		});
@@ -28,16 +23,12 @@ class Pencil_class extends Base_tools_class {
 
 		this.default_events();
 	}
-
 	dragMove(event) {
 		if (config.TOOL.name != this.name)
 			return;
 		this.mousemove(event);
 	}
-
 	pointerdown(e) {
-		// Devices that don't actually support pen pressure can give 0.5 as a false reading.
-		// It is highly unlikely a real pen will read exactly 0.5 at the start of a stroke.
 		if (e.pressure && e.pressure !== 0 && e.pressure !== 0.5 && e.pressure <= 1) {
 			this.pressure_supported = true;
 			this.pointer_pressure = e.pressure;
@@ -45,25 +36,18 @@ class Pencil_class extends Base_tools_class {
 			this.pressure_supported = false;
 		}
 	}
-
 	pointermove(e) {
-		// Pressure of exactly 1 seems to be an input error, sometimes I see it when lifting the pen
-		// off the screen when pressure reading should be near 0.
 		if (this.pressure_supported && e.pressure < 1) {
 			this.pointer_pressure = e.pressure;
 		}
 	}
-
 	mousedown(e) {
 		var mouse = this.get_mouse_info(e);
 		if (mouse.click_valid == false)
 			return;
-
 		var params_hash = this.get_params_hash();
 		var opacity = Math.round(config.ALPHA / 255 * 100);
-		
 		if (config.layer.type != this.name || params_hash != this.params_hash) {
-			//register new object - current layer is not ours or params changed
 			this.layer = {
 				type: this.name,
 				data: [],
@@ -88,7 +72,6 @@ class Pencil_class extends Base_tools_class {
 			this.params_hash = params_hash;
 		}
 		else {
-			//continue adding layer data, just register break
 			const new_data = JSON.parse(JSON.stringify(config.layer.data));
 			new_data.push(null);
 			app.State.do_action(
@@ -100,7 +83,6 @@ class Pencil_class extends Base_tools_class {
 			);
 		}
 	}
-
 	mousemove(e) {
 		var mouse = this.get_mouse_info(e);
 		var params = this.getParams();
@@ -109,16 +91,11 @@ class Pencil_class extends Base_tools_class {
 		if (mouse.click_valid == false) {
 			return;
 		}
-
-		//detect line size
 		var size = params.size;
 		var new_size = size;
-
 		if (params.pressure == true && this.pressure_supported) {
 			new_size = size * this.pointer_pressure * 2;
 		}
-
-		//more data
 		config.layer.data.push([
 			Math.ceil(mouse.x - config.layer.x),
 			Math.ceil(mouse.y - config.layer.y),
@@ -126,7 +103,6 @@ class Pencil_class extends Base_tools_class {
 		]);
 		this.Base_layers.render();
 	}
-
 	mouseup(e) {
 		var mouse = this.get_mouse_info(e);
 		var params = this.getParams();
@@ -134,69 +110,45 @@ class Pencil_class extends Base_tools_class {
 			config.layer.status = null;
 			return;
 		}
-
-		//detect line size
 		var size = params.size;
 		var new_size = size;
-
 		if (params.pressure == true && this.pressure_supported) {
 			new_size = size * this.pointer_pressure * 2;
 		}
-
-		//more data
 		config.layer.data.push([
 			Math.ceil(mouse.x - config.layer.x),
 			Math.ceil(mouse.y - config.layer.y),
 			new_size
 		]);
-
 		this.check_dimensions();
-
 		config.layer.status = null;
 		this.Base_layers.render();
 	}
-
 	render(ctx, layer) {
 		this.render_aliased(ctx, layer);
 	}
-	
-	/**
-	 * draw without antialiasing, sharp, ugly mode.
-	 *
-	 * @param {object} ctx
-	 * @param {object} layer
-	 */
 	render_aliased(ctx, layer) {
 		if (layer.data.length == 0)
 			return;
-
 		var params = layer.params;
 		var data = layer.data;
 		var n = data.length;
 		var size = params.size;
-
-		//set styles
 		ctx.fillStyle = layer.color;
 		ctx.strokeStyle = layer.color;
 		ctx.translate(layer.x, layer.y);
-
-		//draw
 		ctx.beginPath();
 		ctx.moveTo(data[0][0], data[0][1]);
 		for (var i = 1; i < n; i++) {
 			if (data[i] === null) {
-				//break
 				ctx.beginPath();
 			}
 			else {
-				//line
 				size = data[i][2];
 				if(size == undefined){
 					size = 1;
 				}
-
 				if (data[i - 1] == null) {
-					//exception - point
 					ctx.fillRect(
 						data[i][0] - Math.floor(size / 2) - 1,
 						data[i][1] - Math.floor(size / 2) - 1,
@@ -205,7 +157,6 @@ class Pencil_class extends Base_tools_class {
 					);
 				}
 				else {
-					//lines
 					ctx.beginPath();
 					this.draw_simple_line(
 						ctx,
@@ -219,7 +170,6 @@ class Pencil_class extends Base_tools_class {
 			}
 		}
 		if (n == 1 || data[1] == null) {
-			//point
 			ctx.beginPath();
 			ctx.fillRect(
 				data[0][0] - Math.floor(size / 2) - 1,
@@ -228,42 +178,22 @@ class Pencil_class extends Base_tools_class {
 				size
 			);
 		}
-
 		ctx.translate(-layer.x, -layer.y);
 	}
-
-	/**
-	 * draws line without aliasing
-	 *
-	 * @param {object} ctx
-	 * @param {int} from_x
-	 * @param {int} from_y
-	 * @param {int} to_x
-	 * @param {int} to_y
-	 * @param {int} size
-	 */
 	draw_simple_line(ctx, from_x, from_y, to_x, to_y, size) {
 		var dist_x = from_x - to_x;
 		var dist_y = from_y - to_y;
 		var distance = Math.sqrt((dist_x * dist_x) + (dist_y * dist_y));
 		var radiance = Math.atan2(dist_y, dist_x);
-
 		for (var j = 0; j < distance; j++) {
 			var x_tmp = Math.round(to_x + Math.cos(radiance) * j) - Math.floor(size / 2) - 1;
 			var y_tmp = Math.round(to_y + Math.sin(radiance) * j) - Math.floor(size / 2) - 1;
-
 			ctx.fillRect(x_tmp, y_tmp, size, size);
 		}
 	}
-
-	/**
-	 * recalculate layer x, y, width and height values.
-	 */
 	check_dimensions() {
 		if(config.layer.data.length == 0)
 			return;
-
-		//find bounds
 		var data = JSON.parse(JSON.stringify(config.layer.data)); // Deep copy for history
 		var min_x = data[0][0];
 		var min_y = data[0][1];
@@ -277,16 +207,12 @@ class Pencil_class extends Base_tools_class {
 			max_x = Math.max(max_x, data[i][0]);
 			max_y = Math.max(max_y, data[i][1]);
 		}
-
-		//move current data
 		for(var i in data){
 			if(data[i] === null)
 				continue;
 			data[i][0] = data[i][0] - min_x;
 			data[i][1] = data[i][1] - min_y;
 		}
-
-		//change layers bounds
 		app.State.do_action(
 			new app.Actions.Update_layer_action(config.layer.id, {
 				x: config.layer.x + min_x,
@@ -300,7 +226,5 @@ class Pencil_class extends Base_tools_class {
 			}
 		);
 	}
-
 }
-
 export default Pencil_class;
