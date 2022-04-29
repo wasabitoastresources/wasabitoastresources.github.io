35d2e54917025f46455a2a64748195380938dd3a
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 12:55:00 -0500

    Update ellipse.js

--- PhotoEditor/src/js/tools/shapes/ellipse.js
+++ PhotoEditor/src/js/tools/shapes/ellipse.js
@@ -2,9 +2,7 @@ import app from './../../app.js';
 import config from './../../config.js';
 import Base_tools_class from './../../core/base-tools.js';
 import Base_layers_class from './../../core/base-layers.js';
-
 class Ellipse_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
 		this.Base_layers = new Base_layers_class();
@@ -15,21 +13,16 @@ class Ellipse_class extends Base_tools_class {
 		this.snap_line_info = {x: null, y: null};
 		this.mouse_click = {x: null, y: null};
 	}
-
 	load() {
 		this.default_events();
 	}
-
 	mousedown(e) {
 		var mouse = this.get_mouse_info(e);
 		var params = this.getParams();
 		if (mouse.click_valid == false)
 			return;
-
 		var mouse_x = mouse.x;
 		var mouse_y = mouse.y;
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -39,11 +32,8 @@ class Ellipse_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		this.mouse_click.x = mouse_x;
 		this.mouse_click.y = mouse_y;
-
-		//register new object - current layer is not ours or params changed
 		this.layer = {
 			type: this.name,
 			params: this.clone(this.getParams()),
@@ -55,7 +45,6 @@ class Ellipse_class extends Base_tools_class {
 			is_vector: true,
 		};
 		if (params.circle == true) {
-			//disable rotate
 			this.layer.rotate = null;
 		}
 		app.State.do_action(
@@ -64,23 +53,18 @@ class Ellipse_class extends Base_tools_class {
 			])
 		);
 	}
-
 	mousemove(e) {
 		var mouse = this.get_mouse_info(e);
 		var params = this.getParams();
-
 		if (mouse.is_drag == false)
 			return;
 		if (mouse.click_valid == false) {
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -90,12 +74,10 @@ class Ellipse_class extends Base_tools_class {
 				mouse_y = snap_info.y;
 			}
 		}
-
 		var x = Math.min(mouse_x, click_x);
 		var y = Math.min(mouse_y, click_y);
 		var width = Math.abs(mouse_x - click_x);
 		var height = Math.abs(mouse_y - click_y);
-
 		if (params.circle == true || e.ctrlKey == true || e.metaKey) {
 			if (width < height) {
 				width = height;
@@ -110,31 +92,23 @@ class Ellipse_class extends Base_tools_class {
 				y = click_y - height;
 			}
 		}
-
-		//more data
 		config.layer.x = x;
 		config.layer.y = y;
 		config.layer.width = width;
 		config.layer.height = height;
-
 		this.Base_layers.render();
 	}
-
 	mouseup(e) {
 		var mouse = this.get_mouse_info(e);
 		var params = this.getParams();
-
 		if (mouse.click_valid == false) {
 			config.layer.status = null;
 			return;
 		}
-
 		var mouse_x = Math.round(mouse.x);
 		var mouse_y = Math.round(mouse.y);
 		var click_x = Math.round(this.mouse_click.x);
 		var click_y = Math.round(this.mouse_click.y);
-
-		//apply snap
 		var snap_info = this.calc_snap_position(e, mouse_x, mouse_y, config.layer.id);
 		if(snap_info != null){
 			if(snap_info.x != null) {
@@ -145,12 +119,10 @@ class Ellipse_class extends Base_tools_class {
 			}
 		}
 		this.snap_line_info = {x: null, y: null};
-
 		var x = Math.min(mouse_x, click_x);
 		var y = Math.min(mouse_y, click_y);
 		var width = Math.abs(mouse_x - click_x);
 		var height = Math.abs(mouse_y - click_y);
-
 		if (params.circle == true || e.ctrlKey == true || e.metaKey) {
 			if (width < height) {
 				width = height;
@@ -165,14 +137,10 @@ class Ellipse_class extends Base_tools_class {
 				y = click_y - height;
 			}
 		}
-
 		if (width == 0 && height == 0) {
-			//same coordinates - cancel
 			app.State.scrap_last_action();
 			return;
 		}
-
-		//more data
 		app.State.do_action(
 			new app.Actions.Update_layer_action(config.layer.id, {
 				x: x,
@@ -184,22 +152,18 @@ class Ellipse_class extends Base_tools_class {
 			{ merge_with_history: 'new_ellipse_layer' }
 		);
 	}
-
 	render_overlay(ctx){
 		var ctx = this.Base_layers.ctx;
 		this.render_overlay_parent(ctx);
 	}
-
 	demo(ctx, x, y, width, height) {
 		x = parseInt(x);
 		y = parseInt(y);
 		width = parseInt(width);
 		height = parseInt(height);
-
 		ctx.fillStyle = '#aaa';
 		ctx.strokeStyle = '#555';
 		ctx.lineWidth = 3;
-
 		this.ellipse(
 			ctx,
 			x,
@@ -210,13 +174,9 @@ class Ellipse_class extends Base_tools_class {
 			true
 		);
 	}
-
 	render(ctx, layer) {
 		var params = layer.params;
-
 		ctx.save();
-
-		//set styles
 		ctx.strokeStyle = 'transparent';
 		ctx.fillStyle = 'transparent';
 		if(params.border)
@@ -224,17 +184,13 @@ class Ellipse_class extends Base_tools_class {
 		if(params.fill)
 			ctx.fillStyle = params.fill_color;
 		ctx.lineWidth = params.border_size;
-
 		var dist_x = layer.width;
 		var dist_y = layer.height;
-
 		ctx.translate(layer.x + layer.width / 2, layer.y + layer.height / 2);
 		ctx.rotate(layer.rotate * Math.PI / 180);
 		this.ellipse(ctx, -layer.width / 2, -layer.height / 2, layer.width, layer.height, params.border, params.fill);
-
 		ctx.restore();
 	}
-
 	ellipse(ctx, x, y, w, h, stroke, fill) {
 		var kappa = .5522848,
 			ox = (w / 2) * kappa, // control point offset horizontal
@@ -243,7 +199,6 @@ class Ellipse_class extends Base_tools_class {
 			ye = y + h, // y-end
 			xm = x + w / 2, // x-middle
 			ym = y + h / 2; // y-middle
-
 		ctx.beginPath();
 		ctx.moveTo(x, ym);
 		ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
@@ -256,7 +211,5 @@ class Ellipse_class extends Base_tools_class {
 		if (fill == true)
 			ctx.fill();
 	}
-
 }
-
 export default Ellipse_class;
