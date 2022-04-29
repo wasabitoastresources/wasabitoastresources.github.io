95d0a63923faef31b1b83d599ea348ec9b256f85
funschoolmath <103076430+funschoolmath@users.noreply.github.com>
2022-04-29 10:32:15 -0500

    Update shape.js

--- PhotoEditor/src/js/tools/shape.js
+++ PhotoEditor/src/js/tools/shape.js
@@ -4,20 +4,14 @@ import Base_tools_class from './../core/base-tools.js';
 import Base_layers_class from './../core/base-layers.js';
 import Dialog_class from './../libs/popup.js';
 import GUI_tools_class from './../core/gui/gui-tools.js';
-
 var instance = null;
-
 class Shape_class extends Base_tools_class {
-
 	constructor(ctx) {
 		super();
-
-		//singleton
 		if (instance) {
 			return instance;
 		}
 		instance = this;
-
 		this.Base_layers = new Base_layers_class();
 		this.GUI_tools = new GUI_tools_class();
 		this.POP = new Dialog_class();
@@ -26,37 +20,27 @@ class Shape_class extends Base_tools_class {
 		this.layer = {};
 		this.preview_width = 150;
 		this.preview_height = 120;
-
 		this.set_events();
 	}
-
 	set_events() {
 		document.addEventListener('keydown', (event) => {
 			var code = event.keyCode;
 			if (this.Helper.is_input(event.target))
 				return;
-
 			if (code == 72) {
-				//H
 				this.show_shapes();
 			}
 		}, false);
 	}
-
 	load() {
-
 	}
-
 	on_activate() {
 		this.show_shapes();
 	}
-
 	async show_shapes(){
 		var _this = this;
 		var html = '';
-
 		var data = this.get_shapes();
-
 		for (var i in data) {
 			html += '<div class="item">';
 			html += '	<canvas id="c_' + data[i].key + '" width="' + this.preview_width + '" height="'
@@ -68,7 +52,6 @@ class Shape_class extends Base_tools_class {
 		for (var i = 0; i < 4; i++) {
 			html += '<div class="item"></div>';
 		}
-
 		var settings = {
 			title: 'Shapes',
 			className: 'wide',
@@ -77,11 +60,9 @@ class Shape_class extends Base_tools_class {
 				node.classList.add('flex-container');
 				node.innerHTML = html;
 				popup.el.querySelector('.dialog_content').appendChild(node);
-				//events
 				var targets = popup.el.querySelectorAll('.item canvas');
 				for (var i = 0; i < targets.length; i++) {
 					targets[i].addEventListener('click', function (event) {
-						//we have click
 						_this.GUI_tools.activate_tool(this.dataset.key);
 						_this.POP.hide();
 					});
@@ -89,38 +70,26 @@ class Shape_class extends Base_tools_class {
 			},
 		};
 		this.POP.show(settings);
-
-		//sleep, lets wait till DOM is finished
 		await new Promise(r => setTimeout(r, 10));
-
-		//draw demo thumbs
 		for (var i in data) {
 			var function_name = 'demo';
 			var canvas = document.getElementById('c_'+data[i].key);
 			var ctx = canvas.getContext("2d");
-
 			if(typeof data[i].object[function_name] == "undefined")
 				continue;
-
 			data[i].object[function_name](ctx, 20, 20, this.preview_width - 40, this.preview_height - 40, null);
 		}
 	}
-
 	render(ctx, layer) {
-
 	}
-
 	get_shapes(){
 		var list = [];
-
 		for (var i in this.Base_gui.GUI_tools.tools_modules) {
 			var object = this.Base_gui.GUI_tools.tools_modules[i];
 			if (object.full_key.indexOf("shapes/") == -1 )
 				continue;
-
 			list.push(object);
 		}
-
 		list.sort(function(a, b) {
 			var nameA = a.title.toUpperCase();
 			var nameB = b.title.toUpperCase();
@@ -128,10 +97,7 @@ class Shape_class extends Base_tools_class {
 			if (nameA > nameB) return 1;
 			return 0;
 		});
-
 		return list;
 	}
-
 }
-
 export default Shape_class;
