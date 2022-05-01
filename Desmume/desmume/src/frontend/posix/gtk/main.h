670b8eee75a250f2400577e06e9f295493d0cfd2
lol-ye <konnerkaib@gmail.com>
2022-04-30 23:07:39 -0500

    didnt work lol

--- Desmume/desmume/src/frontend/posix/gtk/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DESMUME_GTK_MAIN_H__
-#define __DESMUME_GTK_MAIN_H__
-
-void Pause(GSimpleAction *action, GVariant *parameter, gpointer user_data);
-void Launch(GSimpleAction *action, GVariant *parameter, gpointer user_data);
-
-#endif
